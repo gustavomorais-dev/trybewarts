0047eff4909be3c898cbe5432afbcf05b718918e
Iago Gois <iagorafaelbg@gmail.com>
2023-01-17 17:15:06 -0300

    3 - cria formulario de login e verifica se login e senha estao corretos

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // Trybewarts JS
-console.log('TESTE LINK');
+// Variaveis
+const loginButton = document.querySelector('#login-button');
+
+// Funcoes
+// Login button HEADER
+loginButton.addEventListener('click', () => {
+  const loginEmail = document.querySelector('#login-email');
+  const loginPassword = document.querySelector('#login-password');
+  if (loginEmail.value === 'tryber@teste.com' && loginPassword.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
