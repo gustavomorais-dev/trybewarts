1fd6310bb14af8d7a2f2fe9140ea3ede574a2941
gustavomorais <gustavomorais.dev@gmail.com>
2023-01-17 19:02:47 -0300

    completa requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Trybewarts JS
 // Variaveis
 const loginButton = document.querySelector('#login-button');
+const agreement = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
+
+// Desabilita o botão enviar
+submitButton.disabled = true;
 
 // Funcoes
 // Login button HEADER
@@ -13,3 +18,8 @@ loginButton.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+// Send button disable/enable
+agreement.addEventListener('click', () => {
+agreement.checked ? submitButton.disabled = false : submitButton.disabled = true;
+})
