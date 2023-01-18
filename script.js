4913f8e58dfd9a0fceed64f104542d1b35b0bd65
Iago Gois <iagorafaelbg@gmail.com>
2023-01-17 21:03:18 -0300

    Meio caminho do requisito 21

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const loginButton = document.getElementById('login-button');
 const agreement = document.getElementById('agreement');
 const submit = document.getElementById('submit-btn');
+const getData = document.getElementById('form-data');
+const textArea = document.getElementById('textarea');
 
 // Desabilita o botão enviar
 submit.disabled = true;
@@ -16,7 +18,8 @@ const agreeCheck = () => {
   }
 };
 // Login button HEADER
-loginButton.addEventListener('click', () => {
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
   const loginEmail = document.getElementById('login-email');
   const loginPassword = document.getElementById('login-password');
   if (loginEmail.value === 'tryber@teste.com' && loginPassword.value === '123456') {
@@ -24,6 +27,78 @@ loginButton.addEventListener('click', () => {
   } else {
     alert('Email ou senha inválidos.');
   }
+});
+
+const nomeTodo = () => {
+  const getFirstName = document.getElementById('input-name');
+  const getLastName = document.getElementById('input-lastname');
+  const paragrafo = document.createElement('p');
+  paragrafo.innerHTML = `Nome: ${getFirstName.value} ${getLastName.value}`;
+  getData.appendChild(paragrafo);
+};
+
+const email = () => {
+  const getEmail = document.getElementById('input-email');
+  const paragrafo = document.createElement('p');
+  paragrafo.innerHTML = `Email: ${getEmail.value}`;
+  getData.appendChild(paragrafo);
+};
+
+const casa = () => {
+  const getCasa = document.getElementById('house');
+  const paragrafo = document.createElement('p');
+  paragrafo.innerHTML = `Casa: ${getCasa.value}`;
+  getData.appendChild(paragrafo);
+};
+
+const familia = () => {
+  const getFamilia = document.getElementsByName('family');
+  const paragrafo = document.createElement('p');
+  for (let index = 0; index < getFamilia.length; index += 1) {
+    if (getFamilia[index].checked) {
+      paragrafo.innerHTML = `Família: ${getFamilia[index].value}`;
+    }
+  }
+  getData.appendChild(paragrafo);
+};
+
+const materias = () => {
+  const getMaterias = document.getElementsByClassName('subject');
+  const paragrafo = document.createElement('p');
+  let string = 'Matérias: ';
+  for (let index = 0; index < getMaterias.length; index += 1) {
+    if (getMaterias[index].checked) {
+      string += `${getMaterias[index].value}, `;
+    }
+  }
+  paragrafo.innerHTML = string;
+  getData.appendChild(paragrafo);
+};
+
+const avaliacao = () => {
+
+};
+
+const observacoes = () => {
+  const paragrafo = document.createElement('p');
+  paragrafo.innerHTML = `Observações: ${textArea.value}`;
+  getData.appendChild(paragrafo);
+};
+
+const printData = () => {
+  getData.innerHTML = '';
+  nomeTodo();
+  email();
+  casa();
+  familia();
+  materias();
+  avaliacao();
+  observacoes();
+};
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  printData();
 });
 
 // Send button disable/enable
